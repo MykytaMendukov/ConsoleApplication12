6459737314ab4d1b644477343771b8f06769e4e9
MykytaMendukov <nikitamendukov@gmail.com>
2024-03-28 18:48:38 +0200

    Ex 2

--- ConsoleApplication12/ConsoleApplication12.cpp
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
 using std::cout;
+using std::cin;
+using std::endl;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    int age;
+    try {
+        cout << "Enter your age: ";
+        cin >> age;
+        if (age < 18 && age > 0) {
+            cout << "You are younger 18!" << endl;
+        }
+        else if(age >= 18){
+            cout << "Your are under or 18." << endl;
+        }
+        else{
+            throw -1;
+        }
+    }
+    catch (int err) {
+        cout << "We have some troubles!" << endl;
+    }
 }
