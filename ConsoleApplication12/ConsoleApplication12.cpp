c96a85db35699bfd16280169ee5f28d8796e3f0e
MykytaMendukov <nikitamendukov@gmail.com>
2024-03-28 18:54:21 +0200

    Ex 3

--- ConsoleApplication12/ConsoleApplication12.cpp
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 
 using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+string Backwards(string str) {
+
+	string result;
+
+	for (int i = str.length(); i >= 0; i--) {
+		result.push_back(str[i]);
+	}
+	return result;
+
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   
+	string yourString;
+	
+	cout << "Enter ypur string: ";
+	cin >> yourString;
+
+	cout << "Your string backwards: " << Backwards(yourString) << endl;
+
+	
+	return 0;
 }
